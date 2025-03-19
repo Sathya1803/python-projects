9946a622c81fbdcbc6a7e8b5e5ddfb54fa9892c3
SATHYA R <sathyarajkumar183@gmail.com>
2025-03-20 00:20:02 +0530

    Python Quiz app

---
+++ quiz.py
@@ -0,0 +1,61 @@
+score=0
+print("1.What does CPU stands for?")
+print("a)central processing unit")
+print("b)computer power unit")
+print("c)control processing unit")
+print("d)central performance unit")
+answer1=input("your answer is:").lower()
+if(answer1=='a'):
+    print("✅correct answer!\n")
+    score += 1
+else:
+    print("❌Incorrect! Answer is a)central processing unit\n")
+print("2.What does HTTP stands for?")
+print("a)Hyperlink transfer protocol")
+print("b)Hypertext transfer protocol")
+print("c)Hyper transfer text protocol")
+print("d)Hyperlink text protocol")
+answer2=input("your answer is:").lower()
+if(answer2=='b'):
+    print("✅correct answer!\n")
+    score += 1
+else:
+    print("❌Incorrect!Answer is b)Hypertext transfer protocol\n")
+print("3.What does RAM stands for?")
+print("a)Run after memory")
+print("b)Random access memory")
+print("c)Rapid access memory")
+print("d)Read only memory")
+answer3=input("your answer is:").lower()
+if(answer3=='b'):
+    print("✅correct answer!\n")
+    score += 1
+else:
+     print("❌Incorrect! Answer is b)Random access memory\n")
+print("4.What does IP stands for?")
+print("a)Internet access")
+print("b)Information protocol")
+print("c)Internet protocol")
+print("d)Internal process")
+answer4=input("your answer is:").lower()
+if(answer4=='c'):
+    print("✅correct answer!\n")
+    score += 1
+else:
+     print("❌Incorrect! Answer is c)Internet protocol\n")
+print("5.What does IOT stands for?")
+print("a)Internet of things")
+print("b)Information of things")
+print("c)Internet of technology")
+print("d)Input output transmission")
+answer5=input("your answer is:").lower()
+if(answer5=='a'):
+     print("✅correct answer!\n")
+     score += 1
+else:
+     print("❌Incorrect! Answer is c)Internet of things\n")     
+print("🎉Quiz completed!")
+print(f"your score is:{score}/5")
+        
+        
+
