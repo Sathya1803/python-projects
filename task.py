e7d56fc8e5ca4ef6e427f555d2bc69b9fcb162d7
SATHYA R <sathyarajkumar183@gmail.com>
2025-03-28 23:02:33 +0530

    Band name generator

---
+++ task.py
@@ -0,0 +1,10 @@
+#creating band name generator
+print("Welcome to band generator!")
+#Assigning variable Favorite_actor for actor name
+Favorite_actor=input("who is your favorite actor?\n")
+#Assigning variable Tavorite_color for clor name
+Favorite_color=input("which is your favorite color?\n")
+#Assigning print function to display band name
+print("Your band name is:"+Favorite_actor+" "+Favorite_color+" "+"band")
+
+
